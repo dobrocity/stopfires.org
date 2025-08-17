917fcf0b9c91f29d8a0702ad7bfe1e6e9c07a036
Rinat Enikeev <rinat.enikeev@gmail.com>
2025-08-17 15:49:48 +0300

    add app link

--- src/routes/(marketing)/+layout.svelte
+++ src/routes/(marketing)/+layout.svelte
@@ -21,9 +21,8 @@
           class="border border-primary">★ us on Github</a
         >
       </li>
+      <li class="md:mx-2"><a href="https://app.stopfires.org">App</a></li>
       <li class="md:mx-2"><a href="/contact_us">Contact us</a></li>
-      <!-- <li class="md:mx-2"><a href="/pricing">Pricing</a></li> -->
-      <!-- <li class="md:mx-2"><a href="/account">Account</a></li> -->
       <li class="md:mx-0">
         <a href="/search" aria-label="Search">
           <svg
@@ -62,9 +61,8 @@
         tabindex="0"
         class="menu menu-lg dropdown-content mt-3 z-1 p-2 shadow-sm bg-base-100 rounded-box w-52 font-bold"
       >
+        <li><a href="https://app.stopfires.org">App</a></li>
         <li><a href="/contact_us">Contact us</a></li>
-        <!-- <li><a href="/pricing">Pricing</a></li> -->
-        <!-- <li><a href="/account">Account</a></li> -->
         <li><a href="/search">Search</a></li>
         <li>
           <a
