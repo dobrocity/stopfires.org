29217a31dbbc1df638e7a962641ff63a74775313
Rinat Enikeev <rinat.enikeev@gmail.com>
2025-08-20 16:12:05 +0300

    debug

--- src/routes/.well-known/assetlinks.json/+server.ts
+++ src/routes/.well-known/assetlinks.json/+server.ts
@@ -12,6 +12,7 @@ export const GET: RequestHandler = async () => {
       "namespace": "android_app",
       "package_name": "org.stopfires.app",
       "sha256_cert_fingerprints": [
+        "47:11:22:F9:69:9F:11:6A:AB:B1:AA:0A:18:62:19:DD:99:0A:0A:A6:85:13:3A:F1:FA:53:B3:07:C6:DE:C6:2F",
         "66:F4:18:AA:31:0E:2B:1C:AA:45:CE:25:B2:B2:BD:D9:DA:DA:C1:CB:FE:B4:66:5C:43:74:F9:AC:91:EA:4E:5F"
       ]
     }
